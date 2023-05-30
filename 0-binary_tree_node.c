f281a5b0e40739293de7a51a46299443f9720cf8
TeddyMuli <tmuli974@gmail.com>
2023-05-30 21:09:32 +0300

    ts/CODE/ALX/binary_trees (main)
    ''
    '

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t  - Binary Tree Node
- * 
- * @parent: Pointer to the parent node
- * @value: Integer stored in the node
- * Return: Pointer to the new node, or NULL on failure
-*/
+ * binary_tree_node - Creates a new binary tree node
+ *
+ * @parent: - is a pointer to the parent node of the node to create
+ * @value: - is the value to put in the new node
+ * Return: a pointer to the new node, or NULL on failure
+ */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	/* declaration of the new node with its data type */
+	binary_tree_t *new;
 
-    if (new_node == NULL) {
-        return (NULL);
-    }
+	/* allocate memory space for that node */
+	new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
-    return (new_node);
+	/* assign values to the properties of the node */
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	/* return a pointer to the node */
+	return (new);
 }
